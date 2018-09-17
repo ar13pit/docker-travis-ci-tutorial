39109f12225efb66e9fb5347760ea41f0b716b33
Arpit Aggarwal <ar13pit@gmail.com>
2018-09-17 13:44:11 +0200

    unit test with setup.bash

---
+++ ci/travis-setup.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -e "\e[35m\e[1mUnit test: setup.bash \e[0m"
+
+docker exec arpit-env setup.bash
+
---
+++ setup.bash
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+echo
+echo -e "\e[35m\e[1mTest 1 works \e[0m"
