ac2040370cc5b523ef038769d599c90bb38d9746
Arpit Aggarwal <ar13pit@gmail.com>
2018-09-17 15:59:29 +0200

    run detached container

--- ci/travis-build.bash
+++ ci/travis-build.bash
@@ -3,5 +3,5 @@
 echo -e "\e[35m\e[1mCreating docker image \e[0m"
 
 docker build -t arpit-test .
-docker run -d --name arpit-env arpit-test:latest
+docker run -d --name arpit-env -t arpit-test:latest
 
